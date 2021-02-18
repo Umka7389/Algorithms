08ed064bfccad97e45d2e6da1375c503a1c0081f
Denis <filippovds89@gmail.com>
2021-02-18 13:18:55 +0300

    init

---
+++ src/HW_4/LinkedQueueImpl.java
@@ -0,0 +1,44 @@
+package HW_4;
+
+
+import HW_3.queue.IQueue;
+
+public class LinkedQueueImpl<E> implements IQueue<E> {
+
+    private final TwoSideLinkedList<E> data;
+
+    public LinkedQueueImpl() {
+        this.data = new TwoSideLinkedListImpl<>();
+    }
+
+    @Override
+    public boolean insert(E value) {
+        data.insertLast(value);
+        return true;
+    }
+
+    @Override
+    public E remove() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E peekHead() {
+        return data.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
---
+++ src/HW_4/TwoSideLinkedList.java
@@ -0,0 +1,6 @@
+package HW_4;
+
+public interface TwoSideLinkedList<E> extends LinkedList<E> {
+
+    void insertLast(E value);
+}
