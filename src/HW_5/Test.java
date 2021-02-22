65f7510b496412974454782918b10d350dfe7b42
Denis <filippovds89@gmail.com>
2021-02-22 18:40:49 +0300

    1_done

---
+++ src/HW_5/Test.java
@@ -0,0 +1,8 @@
+package HW_5;
+
+public class Test {
+    public static void main(String[] args) {
+        Exponentiation exp = new Exponentiation();
+        System.out.println(exp.exp(5,3));
+    }
+}
